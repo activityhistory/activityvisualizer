7a28ef4ccec3640db9393c9a12ee38d922edaef2
Jonas <jonas.kemper.biz@gmail.com>
2015-02-04 15:16:02 -0800

    interactive setting of duration

--- d3_vis.js
+++ d3_vis.js
@@ -11,113 +11,81 @@ function drawD3(){
 		return 20 * Math.pow(duration, 1/4);
 	}
 
-	//Create SVG element
-	var svg = d3.select("body")
-	            .append("svg")
-	            .attr("width", w)
-	            .attr("height", h);
-
-	svg.selectAll("circle")
-	   .data(d3_durations)
-	   .enter()
-	   .append("circle")
-		.attr("cx", border_left)
-		.attr("cy", function(d, i) {
-			var sum = 0;
-			for (var j = 0; j < i; j++){
-				sum += 2 * durationToRadius(d3_durations[j]);
-			}
-	           return border_top + sum + durationToRadius(d);
-	   })
-		.attr("r", function(d) {
-	           return durationToRadius(d);
-	   })
-	   .attr("fill", function(d) {
-	        return "rgb(0, 0, " + (d * 10) + ")";
-	   });
-
-	svg.selectAll("text")
-	   .data(d3_durations)
-	   .enter()
-	   .append("text")
-	   .text(function(d, i) {
-	           return d + "min";
-	   })
-	   .attr("y", function(d, i) {
-			var sum = 0;
-			for (var j = 0; j < i; j++){
-				sum += 2 * durationToRadius(d3_durations[j]);
-			}
-	           return border_top + sum + durationToRadius(d);
-	   })
-	   .attr("x", border_left)
-	   .attr("font-family", "sans-serif").attr("font-size", "11px")
-	   .attr("fill", "white");
-
-	svg.selectAll("text_labels")
-	   .data(d3_durations)
-	   .enter()
-	   .append("text")
-	   .text(function(d, i) {
-	           return d3_items[i][0];
-	   })
-	   .attr("y", function(d, i) {
-			var sum = 0;
-			for (var j = 0; j < i; j++){
-				sum += 2 * durationToRadius(d3_durations[j]);
-			}
-	           return border_top + sum + durationToRadius(d);
-	   })
-	   .attr("x", function(d) {
-	           return 2 * border_left;
-	   })
-	   .attr("font-family", "sans-serif").attr("font-size", "11px")
-	   .attr("fill", "black");
-
 	$("#windowSize").slider({ max: 100 },{min:1},{value:50},{slide: function( event, ui ) {
 
-			time_interval = ui.value * 60000;
+		time_interval = ui.value * 60000;
+		d3_durations = [];
+		d3_items = [];
 		tableCreate();
 
+		d3.select("svg")
+			.remove();
 
-            d3.selectAll('circle')
-               .attr("cy", function(d, i) {
-					var sum = 0;
-					for (var j = 0; j < i; j++){
-						sum += 2 * durationToRadius(d3_durations[j]);
-					}
-	           		return border_top + ui.value/50	 * (sum + durationToRadius(d));
-	   			})
-				.attr("r", function(d) {
-	           		return ui.value/50	 * durationToRadius(d);
-	   			});
+		//Create SVG element
+		var svg = d3.select("body")
+			.append("svg")
+			.attr("width", w)
+			.attr("height", h);
+
+		svg.selectAll("circle")
+			.data(d3_durations)
+			.enter()
+			.append("circle")
+			.attr("cx", border_left)
+			.attr("cy", function(d, i) {
+				var sum = 0;
+				for (var j = 0; j < i; j++){
+					sum += 2 * durationToRadius(d3_durations[j]);
+				}
+				return border_top + ui.value/50	 * (sum + durationToRadius(d));
+			})
+			.attr("r", function(d) {
+				return ui.value/50	 * durationToRadius(d);
+			})
+			.attr("fill", function(d) {
+				return "rgb(0, 0, " + (d * 10) + ")";
+			});
+
+		var text_dur = svg.selectAll("text_dur")
+			.data(d3_durations);
+
+		text_dur.enter().append("text")
+			.attr("y", function(d, i) {
+				var sum = 0;
+				for (var j = 0; j < i; j++){
+					sum += 2 * durationToRadius(d3_durations[j]);
+				}
+				return border_top + ui.value/50	 * (sum + durationToRadius(d))
+			})
+			.attr("x", border_left)
+			.attr("font-family", "sans-serif").attr("font-size", "11px")
+			.attr("fill", "white");
+
+		text_dur.text(function(d) { return d + "min"; });
 
 
-			svg.selectAll("text")
-	    .data(d3_durations)
-		.text(function(d) {
-	           return d + "min";
-	   })
-	   .attr("y", function(d, i) {
-			var sum = 0;
-			for (var j = 0; j < i; j++){
-				sum += 2 * durationToRadius(d3_durations[j]);
-			}
-	           return border_top + ui.value/50	 * (sum + durationToRadius(d));
-	   });
+		document.getElementById('sizeText').innerHTML = 'Scaling Factor: ' + ui.value
 
 
 		svg.selectAll("text_labels")
-	   .attr("y", function(d, i) {
-			var sum = 0;
-			for (var j = 0; j < i; j++){
-				sum += 2 * durationToRadius(d3_durations[j]);
-			}
-	           return border_top + ui.value/50	 * (sum + durationToRadius(d));
-	   });
-
-
-            document.getElementById('sizeText').innerHTML = 'Window Size: ' + ui.value
-        }});
+			.data(d3_durations)
+			.enter()
+			.append("text")
+			.text(function(d, i) {
+				return d3_items[i][0];
+			})
+			.attr("y", function(d, i) {
+				var sum = 0;
+				for (var j = 0; j < i; j++){
+					sum += 2 * durationToRadius(d3_durations[j]);
+				}
+				return border_top + sum + durationToRadius(d);
+			})
+			.attr("x", function(d) {
+				return 2 * border_left;
+			})
+			.attr("font-family", "sans-serif").attr("font-size", "11px")
+			.attr("fill", "black");
+	}});
 
 }
