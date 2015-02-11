b834a7f5060a4a010f3d61dea66b3cbd41dc2f95
Jonas <jonas.kemper.biz@gmail.com>
2015-02-11 14:14:36 -0800

    forgot file

---
+++ helpers.js
@@ -0,0 +1,67 @@
+function parseScreenshotNames(){
+    for (var i = 0; i < screenshots.length; i++){
+        var object = {
+            filename : screenshots[i],
+            unix_time : Date.parse(screenshots[i].charAt(2)             //150206-221600413164_1021_601.jpg - > "02.06.2015 22:15:00")//month/day/year
+            + screenshots[i].charAt(3)
+            + "."
+            + screenshots[i].charAt(4)
+            + screenshots[i].charAt(5)
+            + ".20"
+            + screenshots[i].charAt(0)
+            + screenshots[i].charAt(1)
+            + " "
+            + screenshots[i].charAt(7)
+            + screenshots[i].charAt(8)
+            + ":"
+            + screenshots[i].charAt(9)
+            + screenshots[i].charAt(10)
+            + ":"
+            + screenshots[i].charAt(11)
+            + screenshots[i].charAt(12))
+
+        };
+        screenshot_times.push(object);
+    }
+}
+
+
+function convertUnixTimeToHumanReadable(unix_time){
+    var date = new Date(unix_time);
+    var hours = date.getHours();
+    var minutes = "0" + date.getMinutes();
+    var seconds = "0" + date.getSeconds();
+    return hours + ':' + minutes.substr(minutes.length-2) + ':' + seconds.substr(seconds.length-2);
+}
+
+
+
+function inArray(array, id) { //TODO probably don't need two inArray functions
+    for(var i=0;i<array.length;i++) {
+        if (array[i].name == id){
+            return i;
+        }
+    }
+    return -1;
+}
+
+
+function inArray2(array, el) { //TODO not very nice
+    for ( var i = array.length; i--; ) {
+        if ( array[i].name === el ) return true;
+    }
+    return false;
+}
+
+
+function isEqArrays(arr1, arr2) { //TODO not very nice
+    if ( arr1.length != arr2.length ) {
+        return false;
+    }
+    for ( var i = arr1.length; i--; ) {
+        if ( !inArray2( arr2, arr1[i].name ) ) {
+            return false;
+        }
+    }
+    return true;
+}
